5775dd8dbc4fca3c3cda60f3d4a7235614627446
obskyr <powpowd@gmail.com>
2018-03-28 04:44:26 +0200

    Finished graphics decoder!
    Could perhaps do with a little bit of refactoring, but it works (and well, at that)!
    
    I'm really happy with how this layout engine turned out. Probably the most flexible graphics extractor I've seen!

--- tools/source/decodegraphics.cr
+++ tools/source/decodegraphics.cr
@@ -1,14 +1,235 @@
 # Decode Game Boy graphics.
 
 require "option_parser"
+require "stumpy_png"
 
-def error_out(reason)
-    puts "Error: #{reason}"
-    exit 1
+PALETTE_2BPP = {
+    {0xFF, 0xFF, 0xFF},
+    {0xA9, 0xA9, 0xA9},
+    {0x55, 0x55, 0x55},
+    {0x00, 0x00, 0x00}
+}.map { |c| StumpyPNG::RGBA.from_rgb_n(c[0], c[1], c[2], 8) }
+
+PALETTE_1BPP = {
+    {0xFF, 0xFF, 0xFF},
+    {0x00, 0x00, 0x00}
+}.map { |c| StumpyPNG::RGBA.from_rgb_n(c[0], c[1], c[2], 8) }
+
+enum Direction
+    Horizontal
+    Vertical
 end
 
-def parse_layout(s)
-    return nil
+def round_up(num, multiple)
+    return num if multiple == 0
+    remainder = num % multiple
+    return num if remainder == 0
+    return num + multiple - remainder
+end
+
+class GraphicsConversionError < ArgumentError
+end
+
+class Layout
+    @top_level : LayoutLevel
+
+    def initialize(string : String)
+        if !/^\s*((?:H|V)[0-9]*\s*)+$/i.match string
+            raise GraphicsConversionError.new "Invalid layout string format."
+        end
+
+        matches = string.scan /(H|V)([0-9]*)/i
+        prev_level = nil
+        matches.each do |m|
+            direction = m[1].upcase == "H" ? Direction::Horizontal : Direction::Vertical
+            num = m[2].to_i?
+
+            if num == 0
+                raise GraphicsConversionError.new "Invalid layout. 0 is not a valid length."
+            end
+
+            prev_level = LayoutLevel.new direction, num, prev_level
+        end
+
+        @top_level = prev_level.not_nil!
+    end
+
+    def decode(from : IO, to : IO, bit_depth, num_tiles)
+        raise GraphicsConversionError.new "Invalid bit depth." if !(1 <= bit_depth <= 2)
+        raise GraphicsConversionError.new "Number of tiles must be greater than 0." if num_tiles && num_tiles <= 0
+        palette = bit_depth == 2 ? PALETTE_2BPP : PALETTE_1BPP
+        tile_decoder = if bit_depth == 2
+            ->decode_2bpp_tile(IO, StumpyPNG::Canvas, Int32, Int32)
+        else
+            ->decode_1bpp_tile(IO, StumpyPNG::Canvas, Int32, Int32)
+        end
+
+        # The final dimensions of the image need to be determined differently
+        # depending on whether the layout has an infinite dimension or not:
+        # if not, it's just the layout's width and height, but if it does,
+        # the dimensions are based on how much data there is.
+        if @top_level.num
+            if num_tiles && (@top_level.px_width / 8) * (@top_level.px_height / 8) < num_tiles
+                raise GraphicsConversionError.new "Layout dimensions too small to fit #{num_tiles} tiles."
+            end
+            width  = @top_level.px_width
+            height = @top_level.px_height
+        else
+            original_from = from
+            if num_tiles
+                num_pixels = 8 * 8 * num_tiles
+                num_bytes = bit_depth * (num_pixels / 8)
+                bytes = Bytes.new num_bytes
+                bytes_read = original_from.read bytes
+                
+                # If the bytes read stop in the middle of a tile, the tile
+                # decoder will still be able to read that tile. Therefore,
+                # missing a few bytes of the last tile is acceptable.
+                readable_bytes = round_up bytes_read, bit_depth * 8
+                if readable_bytes < num_bytes
+                    raise GraphicsConversionError.new "There are less than #{num_tiles} tiles in the input data."
+                end
+
+                from = IO::Memory.new bytes[0, bytes_read]
+            else
+                from = IO::Memory.new
+                IO.copy original_from, from
+                bytes_read = from.tell
+                num_pixels = ((bytes_read + bit_depth - 1) / bit_depth) * 8
+
+                if num_pixels == 0
+                    raise GraphicsConversionError.new "No data to decode."
+                end
+
+                from.seek 0
+            end
+
+            pixels_in_top_chunk = @top_level.px_width * @top_level.px_height
+            num_pixels = round_up num_pixels, pixels_in_top_chunk
+
+            width  = (@top_level.is_horizontal) ? num_pixels / @top_level.px_height : @top_level.px_width
+            height = (@top_level.is_vertical)   ? num_pixels / @top_level.px_width  : @top_level.px_height
+        end
+
+        canvas = StumpyPNG::Canvas.new width, height
+
+        @top_level.decode from, canvas, tile_decoder, num_tiles, 0, 0
+
+        StumpyPNG.write canvas, to
+    end
+end
+
+class LayoutLevel
+    property direction
+    property num
+    property px_width  : Int32
+    property px_height : Int32
+
+    def is_horizontal
+        return @direction == Direction::Horizontal
+    end
+
+    def is_vertical
+        return @direction == Direction::Vertical
+    end
+
+    def initialize(@direction : Direction, num : Int32?, child : LayoutLevel?)
+        @num = num
+        @child = child
+
+        if child && !child.num
+            raise GraphicsConversionError.new %("Invalid layout. Infinite layout levels ("H" or "V") must be at the end.)
+        end
+
+        child_width  = child ? child.px_width  : 8
+        child_height = child ? child.px_height : 8
+        horizontal_children = num ? (is_horizontal ? num : 1) : 1
+        vertical_children   = num ? (is_vertical   ? num : 1) : 1
+        @px_width  = child_width  * horizontal_children
+        @px_height = child_height * vertical_children
+    end
+
+    def decode(from : IO, canvas : StumpyPNG::Canvas, tile_decoder, num_tiles, x, y)
+        child = @child
+        child_width  = child ? child.px_width  : 8
+        child_height = child ? child.px_height : 8
+        total_decoded = 0
+
+        # It feels like there should be a better way to make an infinite
+        # iterator than `1.times.cycle`, but `loop` can't be assigned...
+        times = (num = @num) ? num.times : 1.times.cycle
+        times.each do
+            if child
+                tiles_left = num_tiles ? num_tiles - total_decoded : nil
+                cur_decoded = child.decode(from, canvas, tile_decoder, tiles_left, x, y)
+            else
+                cur_decoded = tile_decoder.call(from, canvas, x, y)
+            end
+            break if cur_decoded == 0
+
+            x += child_width  if is_horizontal
+            y += child_height if is_vertical
+
+            total_decoded += cur_decoded
+            break if total_decoded == num_tiles
+        end
+
+        return total_decoded
+    end
+end
+
+def decode_2bpp_tile(from : IO, canvas : StumpyPNG::Canvas, x, y)
+    tile = Bytes.new 2 * 8 # 2 bytes per row * 8 rows
+    bytes_read = from.read tile
+    return 0 if bytes_read == 0
+    tile_io = IO::Memory.new tile[0, bytes_read]
+
+    (y...y + 8).each do |cur_y|
+        # If data stops in the middle of a tile, the rest of the tile
+        # should still be filled with something. 0x00, for example.
+        byte_1 = tile_io.read_byte || 0x00
+        byte_2 = tile_io.read_byte || 0x00
+
+        (x...x + 8).each.zip((0...8).reverse_each).each do |cur_x, low_shift_distance|
+            i = ((byte_1 >> low_shift_distance) & 0b1) | (((byte_2 >> low_shift_distance) << 1) & 0b10)
+            canvas[cur_x, cur_y] = PALETTE_2BPP[i]
+        end
+    end
+
+    return 1
+end
+
+def decode_1bpp_tile(from : IO, canvas : StumpyPNG::Canvas, x, y)
+    tile = Bytes.new 1 * 8 # 1 byte per row * 8 rows
+    bytes_read = from.read tile
+    return 0 if bytes_read == 0
+    tile_io = IO::Memory.new tile[0, bytes_read]
+    
+    (y...y + 8).each do |cur_y|
+        # Same as with 2BPP - nonexistent data should be filled out.
+        byte = tile_io.read_byte || 0x00
+
+        (x...x + 8).each.zip((0...8).reverse_each).each do |cur_x, shift_distance|
+            i = (byte >> shift_distance) & 0b1
+            canvas[cur_x, cur_y] = PALETTE_1BPP[i]
+        end
+    end
+
+    return 1
+end
+
+def decode(from, to, layout, bit_depth, num_tiles)
+    layout = Layout.new layout
+    layout.decode(from, to, bit_depth, num_tiles)
+end
+
+
+
+# Front-end logic follows.
+
+def error_out(reason)
+    STDERR.puts "Error: #{reason}"
+    exit 1
 end
 
 in_path = nil
@@ -22,45 +243,51 @@ width = nil
 height = nil
 layout = nil
 
+program_name = PROGRAM_NAME.split('/')[-1]
+usage_line = "Usage: #{program_name} <-l layout | -W width | -H height> [other options...]"
+
 details =
-"Layout:
+%(Layout:
     In order to lay tiles out as you want, you can describe layouts using
-    the `-l` / `--layout` option. Set it to a list of direction-length pairs.
+    the "-l" / "--layout" option. Set it to a list of direction-length pairs.
     
-    Direction-length pairs look something like \"H8\" (8 horizontally) or
-    \"V2\" (2 vertically). They consist of a direction (\"H\" or \"V\" for
+    Direction-length pairs look something like "H8" (8 horizontally) or
+    "V2" (2 vertically). They consist of a direction ("H" or "V" for
     horizontal or vertical) and then a length. Each pair depends on the
     previous one - the first pair specifies which direction to add tiles
     and how many, and the next specifies which direction to stack those
-    \"chunks\" and how many. The one after that specifies how to stack *those*,
+    "chunks" and how many. The one after that specifies how to stack *those*,
     and so on.
 
-    Optionally, the last pair can leave the length out (just be \"H\" or \"V\")
+    Optionally, the last pair can leave the length out (just be "H" or "V")
     and graphics will be added in that direction until the end of the data.
 
-    For example, `--layout \"V2 H4 V2 V\"` will:
+    For example, "--layout 'V2 H4 V2 V'" will:
         1. Decode 2 tiles vertically into a 1x2 chunk
         2. Do that 4 times and add those horizontally into a 4x2 chunk
         3. Do that 2 times and add *those* vertically into a 4x4 chunk
         4. Keep adding 4x4 chunks like that vertically until the data ends.
     
     To simply decode an image of specific dimensions and nothing more,
-    a layout like \"H8 V8\" (8x8 tiles) can be used.
+    a layout like "H8 V8" (8x8 tiles) can be used.
 
-    --width and --height are aliases for the layouts \"H[width] V\" and
-    \"V[height] H\", respectively, or \"\" if both are set.
-"
+    The options "-W" / "--width" and "-H" / "--height" are aliases for
+    the layouts "H[width] V" and "V[height] H", respectively.
+)
+
+if ARGV.size == 0
+    STDERR.puts %(#{usage_line}\nRun "#{program_name} --help" for more options and info!)
+    exit 1
+end
 
 OptionParser.parse! do |parser|
-    program_name = PROGRAM_NAME.split('/')[-1]
-
-    parser.banner = "Usage: #{program_name} [arguments]\n\nArguments:"
+    parser.banner = "#{usage_line}\n\nArguments:"
     
     parser.on("-h", "--help", "Show this help and exit.\n") do
         puts parser
         puts
         puts details
-        exit 1
+        exit 0
     end
     
     parser.on("-i PATH", "Input file. If unspecified, " \
@@ -91,14 +318,14 @@ OptionParser.parse! do |parser|
         "Set the bit depth (either 1 or 2; default 2)."
     ) do |d|
         bit_depth = d.to_i
-        raise ArgumentError.new "Invalid bit depth" if !(1 <= bit_depth <= 2)
+        raise ArgumentError.new "Invalid bit depth." if !(1 <= bit_depth <= 2)
     rescue ArgumentError
         error_out "Invalid bit depth. Must be either 1 or 2."
     end
 
     parser.on(
         "-W WIDTH", "--width WIDTH",
-        "Add tiles horizontally and wrap to the next row after `WIDTH` tiles."
+        "Add tiles horizontally and wrap to the next row after WIDTH tiles."
     ) do |w|
         width = w.to_i(prefix: true)
     rescue ArgumentError
@@ -107,7 +334,7 @@ OptionParser.parse! do |parser|
 
     parser.on(
         "-H HEIGHT", "--height HEIGHT",
-        "Add tiles vertically and wrap to the next column after `HEIGHT` tiles."
+        "Add tiles vertically and wrap to the next column after HEIGHT tiles."
     ) do |h|
         height = h.to_i(prefix: true)
     rescue ArgumentError
@@ -116,19 +343,23 @@ OptionParser.parse! do |parser|
 
     parser.on(
         "-l LAYOUT", "--layout LAYOUT",
-        "Set the layout of the tiles - see the \"Layout\" section."
+        %(Set the layout of the tiles - see the "Layout" section.)
     ) do |l|
-        layout = parse_layout layout
+        layout = l
     rescue ArgumentError
-        error_out "Invalid layout. Run `#{program_name} -h` for help!"
+        error_out %(Invalid layout. Run "#{program_name} -h" for help!)
     end
     
     parser.missing_option do |option|
-        error_out "#{option} is missing an argument. Run `#{program_name} -h` for help!"
+        error_out %(#{option} is missing an argument. Run "#{program_name} -h" for help!)
     end
     parser.invalid_option do |option|
-        error_out "Invalid option: #{option}. Run `#{program_name} -h` for help!"
+        error_out %(Invalid option: #{option}. Run "#{program_name} -h" for help!)
     end
+end
+
+if !(layout || width || height)
+    error_out %(No dimensions set! Sey either "--layout", "--width", or "--height".)
 end
 
 if layout && (width || height) 
@@ -136,7 +367,48 @@ if layout && (width || height)
 end
 
 if width && height
-    error_out "Can't set both width and height options at the same time.\n" \
-              "Use a --layout of either \"H#{width} V#{height}\" (rows) or " \
-              "\"V#{height} H#{width}\" (columns), depending on which you want!"
+    error_out %(Can't set both width and height options at the same time.\n) \
+              %(Use a --layout of either "H#{width} V#{height}" (rows) or ) \
+              %("V#{height} H#{width}" (columns), depending on which you want!)
 end
+
+if width
+    layout = "H#{width} V"
+elsif height
+    layout = "V#{height} H"
+end
+
+if path = in_path
+    begin
+        in_io = File.open path, "rb"
+    rescue Errno
+        error_out "Couldn't open #{in_path} for reading."
+    end
+else
+    in_io = STDIN
+end
+
+if path = out_path
+    begin
+        out_io = File.open path, "wb"
+    rescue Errno
+        error_out "Couldn't open #{out_path} for writing."
+    end
+else
+    STDOUT.flush_on_newline = false
+    out_io = STDOUT
+end
+
+begin
+    in_io.skip offset
+rescue IO::EOFError
+end
+
+begin
+    decode(in_io, out_io, layout.not_nil!, bit_depth, num_tiles)
+rescue e: GraphicsConversionError
+    error_out e.message
+end
+
+in_io.close  if in_path
+out_io.close if out_path
